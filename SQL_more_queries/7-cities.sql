f47fd742cb6df8191656312f1a23ae5ceaa699d6
Hend-Moustafa18 <hend.moustafa18@icloud.com>
2023-12-28 11:29:49 +0200

    new

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,10 @@
+-- Create the database hbtn_0d_usa  if it doesn't exist
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+-- Creates the table cities.
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
+    `id`   INT  UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    `state_id` INT NOT NULL,
+    FOREIGN KEY(`state_id`)
+    REFERENCES `hbtn_0d_usa`.`states`(`id`),
+    `name` VARCHAR(256) NOT NULL
+);
