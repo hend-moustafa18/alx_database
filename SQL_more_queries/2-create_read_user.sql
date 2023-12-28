2fc0689436ad096e6e87166c379b3d79ebe0bd5e
Hend-Moustafa18 <hend.moustafa18@icloud.com>
2023-12-28 11:02:57 +0200

    NEW

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,11 @@
+-- Create the database hbtn_0d_2 if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create the user user_0d_2 if it doesn't exist
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant SELECT privilege on hbtn_0d_2 to user_0d_2
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
+-- Flush privileges to apply the changes
+FLUSH PRIVILEGES;
