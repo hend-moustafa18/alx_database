32d306ec7bbe688e9280f53ac84a716f872cf246
Hend-Moustafa18 <hend.moustafa18@icloud.com>
2023-12-28 10:58:57 +0200

    NEW

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,8 @@
--- Creates the user user_0d_1 with all privileges.
-CREATE USER
-    IF NOT EXISTS 'user_0d_1'@'localhost'
+-- Create the user user_0d_1 if it doesn't exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- Grant all privileges to user_0d_1
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- Flush privileges to apply the changes
+FLUSH PRIVILEGES;
